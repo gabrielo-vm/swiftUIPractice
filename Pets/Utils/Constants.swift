fe9f8b533486026fa3628a2869cc2d70372c5a9c
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-27 13:43:03 -0700

    Added gitignore file to ignore xcodeproj/project.pbxproj

---
+++ Pets/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Pets
+//
+//  Created by Gabriel Valdez Mendoza on 27/03/23.
+//
+
+import Foundation
---
+++ Pets/Webservice.swift
@@ -0,0 +1,8 @@
+//
+//  Webservice.swift
+//  Pets
+//
+//  Created by Gabriel Valdez Mendoza on 27/03/23.
+//
+
+import Foundation
