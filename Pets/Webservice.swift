c510d17e531dae77631dfa1dd38d5c4b0cbc43c1
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-28 08:54:25 -0700

    Added webservice class with load func and added enum for NetworkError

--- Pets/Webservice.swift
+++ Pets/Webservice.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidServerResponce
+}
+
+class Webservice {
+    
+    func load(resource: String ) async throws{
+        guard let url = URL(string: resource) else {
+            throw NetworkError.invalidURL
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+                throw NetworkError.invalidServerResponce
+        }
+        
+        
+    }
+}
