f7b301c4e9cd34dd22d45049338f0d1766d24d6d
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-28 14:00:18 -0700

    Added PetListViewModel

--- Pets/View Models/PetListViewModel.swift
+++ Pets/View Models/PetListViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class PetListViewModel: ObservableObject {
+    
+    private var service: Webservice
+    
+    @Published var components: [UIComponent] = []
+    
+    init(service: Webservice){
+        self.service = service
+    }
+    
+    func load() async {
+        do {
+            let screenModel = try await service.load(resource: Constants.Urls.petListing)
+            components = try screenModel.buildComponents()
+            
+        } catch{
+            print(error)
+        }
+    }
+}
