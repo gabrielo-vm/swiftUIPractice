4cb1c7f157e60eb6ee0e343deb710d9d68cf2454
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-28 10:40:22 -0700

    added FeaturedImageComponent whicj confirms to UIComponent

--- Pets/Components/FeaturedImageComponent.swift
+++ Pets/Components/FeaturedImageComponent.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct FeaturedImageComponent: UIComponent {
+    
+    let uiModel: FeaturedImageUIModel
+    
+    var uniqueID: String {
+        return ComponentType.featuredImage.rawValue
+    }
+    
+    func render() -> AnyView {
+//        AnyView(Text("Hello"))
+        AsyncImage(url: uiModel.imageUrl) { image in
+            
+            image.resizable()
+                .aspectRatio(contentMode: .fill)
+                    
+        } placeholder: {
+             ProgressView()
+        }.toAnyView()
+    }
+    
+}
+    
