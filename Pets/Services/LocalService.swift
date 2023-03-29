875269109fe19c60033f48fcc3bb09c36b7bd396
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-29 15:43:23 -0700

    added local service to read from json

--- Pets/Services/LocalService.swift
+++ Pets/Services/LocalService.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class LocalService: ServiceProtocol {
+    
+    func load(_ resourceName: String) async throws -> ScreenModel {
+        
+        guard let path = Bundle.main.path(forResource: resourceName, ofType: "json")else {
+            fatalError("Resourse file \(resourceName) not found!")
+        }
+        
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        let screenModel = try JSONDecoder().decode(ScreenModel.self, from: data)
+        
+        return screenModel
+    }
+    
+    
+}
