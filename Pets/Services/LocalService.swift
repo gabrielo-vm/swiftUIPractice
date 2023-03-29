8c2314b5eddadeb32072ea1d0c4f04e5e9525a17
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-29 15:42:22 -0700

    Added json to mock data for pet-listing

---
+++ Pets/Services/LocalService.swift
@@ -0,0 +1,8 @@
+//
+//  LocalService.swift
+//  Pets
+//
+//  Created by Gabriel Valdez Mendoza on 29/03/23.
+//
+
+import Foundation
---
+++ Pets/Services/ServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceProtocol.swift
+//  Pets
+//
+//  Created by Gabriel Valdez Mendoza on 29/03/23.
+//
+
+import Foundation
