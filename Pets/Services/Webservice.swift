645eb6429ef49120abda32940c20948c23f92088
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-29 15:46:30 -0700

    modified webservice to confirm to ServiceProtocol

--- Pets/Services/Webservice.swift
+++ Pets/Services/Webservice.swift
@@ -13,19 +13,18 @@ enum NetworkError: Error {
     case invalidServerResponce
 }
 
-class Webservice {
-    
-    func load(resource: String ) async throws -> ScreenModel{
-        guard let url = URL(string: resource) else {
-            throw NetworkError.invalidURL
-        }
-        let (data, response) = try await URLSession.shared.data(from: url)
+class Webservice: ServiceProtocol{
         
-        guard let httpResponse = response as? HTTPURLResponse,
+        func load(_ resource: String ) async throws -> ScreenModel{
+            guard let url = Constants.ScreenResource.resource(for: resource) else {
+                throw NetworkError.invalidURL
+            }
+            let (data, response) = try await URLSession.shared.data(from: url)
+            guard let httpResponse = response as? HTTPURLResponse,
               httpResponse.statusCode == 200 else {
                 throw NetworkError.invalidServerResponce
         }
-        print(data,"check")
+        
         return try JSONDecoder().decode(ScreenModel.self, from: data)
     }
 }
