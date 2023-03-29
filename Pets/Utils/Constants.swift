32914637c498d6a0ff3c34e25f7cbb48ad4f4c2a
Gabriel Valdez Mendoza <gabriel.valdez.mendoza.-nd@disney.com>
2023-03-29 15:48:48 -0700

    Added ScreenResource to constants which returns url constructed with urlcomponents

--- Pets/Utils/Constants.swift
+++ Pets/Utils/Constants.swift
@@ -9,9 +9,30 @@ import Foundation
 
 struct Constants {
     
-    struct Urls {
-        static let baseURL = ""
+    
+    struct ScreenResource {
+        
+        static let host = "localhost"
         static let petListing = "pet-listing"
+        
+        static func resource(for resourceName: String)  -> URL? {
+            
+            var components = URLComponents()
+            components.scheme = "http"
+            components.host = host
+            components.port = 3000
+            components.path = "/\(resourceName)"
+
+            return components.url
+        }
+        
+        
+    }
+    
+    
+    struct Urls {
+        static let baseURL = "http://localhost:3000"
+        static let petListing = "\(baseURL)/pet-listing"
     }
     
 }
